04c0c940c955301f953ecbe9739e6751878b9d6e
VKGarg7 <141575374+VKGarg7@users.noreply.github.com>
2024-07-10 16:40:57 +0000

    main.jsx

---
+++ gemini-clone/src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import react from "react"
+import './Main.css'
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Main;
