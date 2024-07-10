fa41d9138158209a6304736e696fcdf5a7414a17
VKGarg7 <141575374+VKGarg7@users.noreply.github.com>
2024-07-10 12:26:09 +0000

    Sidebar

---
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Sidebar.css"
+import {assets} from "../../assets"
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
