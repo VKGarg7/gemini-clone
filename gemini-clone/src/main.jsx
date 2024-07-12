0f8a9f9f4e354bb11c563f08b97ae8539ae03e3e
VKGarg7 <141575374+VKGarg7@users.noreply.github.com>
2024-07-12 15:48:23 +0000

    main.jsx

--- gemini-clone/src/main.jsx
+++ gemini-clone/src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
-import "./index.css";
-import ContextProvider from "./context/Context.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </ContextProvider>,
-);
+  </React.StrictMode>,
+)
